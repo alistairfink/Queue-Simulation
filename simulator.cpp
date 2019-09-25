4a6b001010b856df83135f3d0208a51495b780b0
Alistair Fink <alistairfink@gmail.com>
2019-09-24 22:00:32 -0400

    fix simulator

--- simulator.cpp
+++ simulator.cpp
@@ -12,86 +12,86 @@
 using namespace std;
 
 // Event Struct and Functions
- Event init_event(char type, float time, int len){
+//  Event init_event(char type, float time, int len){
 	
-	  Event event;
-	  float lambda = 1.0/len;
-	  vector<float> tmp = generator(lambda,1);
+// 	  Event event;
+// 	  float lambda = 1.0/len;
+// 	  vector<float> tmp = generator(lambda,1);
 	
-		if (type == 'a')
-		{			
-			event.len = tmp.pop_back();
-		}
-		else
-		{
-			event.len = len;
-		}
-		event.time = time;
-		event.type = type;
+// 		if (type == 'a')
+// 		{			
+// 			event.len = tmp.pop_back();
+// 		}
+// 		else
+// 		{
+// 			event.len = len;
+// 		}
+// 		event.time = time;
+// 		event.type = type;
 		
-		return event;
-}
+// 		return event;
+// }
 
 
-void print_event(struct Event E){
- 	cout << "\n Event Type : " << E.type << "\n Event Time : " << E.time << "\n Event Length: %f " << E.len << endl;
-}
+// void print_event(struct Event E){
+//  	cout << "\n Event Type : " << E.type << "\n Event Time : " << E.time << "\n Event Length: %f " << E.len << endl;
+// }
 
-void print_event_list(vector<Event> vect){
+// void print_event_list(vector<Event> vect){
     
-	if(vect.empty())
-        cout << "No events in the Queue.\n";
-    else{
-    		for (int i = 0; i < vect.size(); i++)                          
-	            {
-	                cout << "\n Event Type : " << vect[i].type << "\n Event Time : " << vect[i].time << "\n Event Length: %f " << vect[i].len;
-				}
-	}  
-}
+// 	if(vect.empty())
+//         cout << "No events in the Queue.\n";
+//     else{
+//     		for (int i = 0; i < vect.size(); i++)                          
+// 	            {
+// 	                cout << "\n Event Type : " << vect[i].type << "\n Event Time : " << vect[i].time << "\n Event Length: %f " << vect[i].len;
+// 				}
+// 	}  
+// }
  
-	//Discrete Event Simulation Functions
+// 	//Discrete Event Simulation Functions
  
- Event observers(int alpha, int duration)
-{
- 	Event e;
-	float curr_time=0;
- 	float tmp_time;
+//  Event observers(int alpha, int duration)
+// {
+//  	Event e;
+// 	float curr_time=0;
+//  	float tmp_time;
 
-	while (curr_time <= duration) {
+// 	while (curr_time <= duration) {
 		
-		vector<float> tmp;
-		tmp = generator(alpha,1);
-		float gen_time = tmp.pop_back();
-		curr_time += gen_time; 
+// 		vector<float> tmp;
+// 		tmp = generator(alpha,1);
+// 		float gen_time = tmp.pop_back();
+// 		curr_time += gen_time; 
 		
-		if (curr_time > duration) {
-			break;
-		}
+// 		if (curr_time > duration) {
+// 			break;
+// 		}
 
-		e = init_event('O', curr_time, 0);
-		return e;
-	}
+// 		e = init_event('O', curr_time, 0);
+// 		return e;
+// 	}
 
-}
+// }
 
-  Event arrivals(float lambda , int len_packet, int duration ){
- 	float curr_time = 0;
-	Event e;
+//   Event arrivals(float lambda , int len_packet, int duration ){
+//  	float curr_time = 0;
+// 	Event e;
 
-	while (curr_time <= duration) {
-		vector<float> tmp;
-		tmp= generator(lambda,1);
-		float gen_time = tmp.push_back();
-		curr_time += gen_time;
+// 	while (curr_time <= duration) {
+// 		vector<float> tmp;
+// 		tmp= generator(lambda,1);
+// 		float gen_time = tmp.push_back();
+// 		curr_time += gen_time;
 	
-		if (curr_time > duration) {
-			break;
-		}
+// 		if (curr_time > duration) {
+// 			break;
+// 		}
 
-		e = init_event('A', curr_time, len_packet);
-		return e;
-	}
-}
+// 		e = init_event('A', curr_time, len_packet);
+// 		return e;
+// 	}
+// }
 
 struct result_parameters simulate(vector<Event> vect, int C) {
 
@@ -103,8 +103,11 @@ struct result_parameters simulate(vector<Event> vect, int C) {
 	int curr_service_time=0;
 	int last_arrival_time = 0;
 
+	int packets_in_buffer = 0;
+	float idle_time = 0.0;
+	float prev_event = 0.0;
 	// Run the simulator
-	for (int i = 0; i < vect.size; i++) {
+	for (int i = 0; i < vect.size(); i++) {
 
 		//Arrival Event
 		if (vect[i].type == 'a') {
@@ -114,6 +117,10 @@ struct result_parameters simulate(vector<Event> vect, int C) {
 		//Observer Event
 		else if (vect[i].type == 'o') {
 			observer_counter++;
+			packets_in_buffer += arrivals_counter - departure_counter;
+			if(arrivals_counter == departure_counter) {
+				idle_time += vect[i].time - prev_event;
+			}
 		}
 		//Departure Event
 		else if (vect[i].type == 'd') {
@@ -124,6 +131,8 @@ struct result_parameters simulate(vector<Event> vect, int C) {
 		else {
 			cout << "invalid Event Type" << endl;
 		}
+
+		prev_event = vect[i].time;
 	}
 	
 	test_result.packet_arrivals = arrivals_counter;
@@ -131,29 +140,23 @@ struct result_parameters simulate(vector<Event> vect, int C) {
 	test_result.packet_departures = departure_counter;
 
 	//calculate departure time  lengBits/tranmission rate+arrival time
-	int simulation_time = T; 		
+	// int simulation_time = T; 		
 	int transmission_rate = C;
-	float roh = p;	  		
-	int len_packet = L;
+	// float roh = p;	  		
+	// int len_packet = L;
 	
-	float lambda = (roh / len_packet) * transmission_rate; 
-	float alpha = lambda + 5;
+	// float lambda = (roh / len_packet) * transmission_rate; 
+	// float alpha = lambda + 5;
 
-	cout<<"T : %d seconds /n"<<endl;
-	cout<<"K : %d  /n"<<endl;
-	cout<<"C : %d /n"<<endl;
-	cout<<"Alpha : %f /n"<<endl;
-	cout<<"Roh : %f /n "<<endl;
-	cout<<"Lambda %f\n " <<endl;
-	cout<<"L : %d /n"<<endl;
-	
-	test_result.duration = T;
-	test_result.alpha = alpha;
-	test_result.lambda = lambda;
-	test_result.len_packet = len_packet;
+	// test_result.duration = T;
+	// test_result.alpha = alpha;
+	// test_result.lambda = lambda;
+	// test_result.len_packet = len_packet;
 	test_result.link_rate = transmission_rate;
-	test_result.size_buffer = buffer_size;
-	test_result.roh = roh;
+	test_result.avg_packets = float(packets_in_buffer) / float(observer_counter);
+	test_result.p_idle = idle_time / prev_event;
+	// test_result.size_buffer = buffer_size;
+	// test_result.roh = roh;
 
 	return test_result;
 }
@@ -244,12 +247,12 @@ result_parameters simulate_finite_buffer(vector<Event> events, int buffer_size, 
 	return results;
 }
 
-int main()
-{
+// int main()
+// {
 	
-	Event t_event;
-	t_event = init_event('A',5,10);
-	print_event(t_event);
-	cout<<"test";
-	return 0;
-}
+// 	Event t_event;
+// 	t_event = init_event('A',5,10);
+// 	print_event(t_event);
+// 	cout<<"test";
+// 	return 0;
+// }
