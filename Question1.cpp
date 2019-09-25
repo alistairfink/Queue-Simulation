535c973b4b2ced1cb73254bda97283c60970ee05
Joe <youssefroman96@gmail.com>
2019-09-24 21:41:38 -0400

    Updated Simulator

--- Question1.cpp
+++ Question1.cpp
@@ -8,22 +8,22 @@
 using namespace std;
 
 int main() {
-    // Generate values and print them.
-    vector<float> result = generator(75, 1000);
-    float total = 0;
-    for(std::size_t i=0; i < result.size(); ++i) {
-        cout << result[i] << endl;
-        total += result[i];
-    }
-
-    float mean = total / 1000.0;
-    cout << "Mean: " << mean << endl;
-
-    float variance = 0;
-    for (std::size_t i = 0; i < result.size(); ++i) {
-    	variance += (result[i] - mean)*(result[i] - mean);
+	// Generate values and print them.
+	vector<float> result = generator(75, 1000);
+	float total = 0;
+	for (std::size_t i = 0; i < result.size(); ++i) {
+		cout << result[i] << endl;
+		total += result[i];
 	}
 
-    cout << "Variance: " << variance/1000 << endl;
-    return -1;
+	float mean = total / 1000.0;
+	cout << "Mean: " << mean << endl;
+
+	float variance = 0;
+	for (std::size_t i = 0; i < result.size(); ++i) {
+		variance += (result[i] - mean)*(result[i] - mean);
+	}
+
+	cout << "Variance: " << variance / 1000 << endl;
+	return -1;
 }
